525a38e7ef51cb50fd97a7c6c1d8be3b88509618
nguyenhothanhtam <thanhtam.nguyenho@gmail.com>
2018-11-29 20:46:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thanh/piano1/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.thanh.piano1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000; // delay 4s
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
